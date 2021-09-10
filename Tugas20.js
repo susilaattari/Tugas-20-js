e3203b88ae8dd641feb01145dca91375ae90de4d
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-11 03:09:21 +0700

    Tugas 20 javascript Niomic

---
+++ Tugas20.js
@@ -0,0 +1,8 @@
+function StringMatch(){
+    let page =  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+    console.log(page.match('are'));
+    console.log(page.match('9'));
+    console.log(page.match('words'));
+
+}
+StringMatch()
